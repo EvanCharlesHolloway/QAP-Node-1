76d03684e19d30d08273037cb2036177f521c132
EvanCharlesHolloway <128486114+EvanCharlesHolloway@users.noreply.github.com>
2023-09-27 09:04:24 -0230

    Delete app.mjs
    this was an extra I forgot to delete

--- app.mjs
+++
@@ -1,7 +0,0 @@
-// Import the chalk package
-import chalk from 'chalk';
-
-// Use chalk to colorize text
-console.log(chalk.blue('This text is blue.'));
-console.log(chalk.red('This text is red.'));
-console.log(chalk.green('This text is green.'));
